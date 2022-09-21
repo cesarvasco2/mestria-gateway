7613056d593cb709fa20b14caf744fead4c71b62
cesarvasco2 <70148129+cesarvasco2@users.noreply.github.com>
2022-09-21 10:57:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,12 @@ def on_message(client, userdata, msg):
     #print(dict_payload['DATA'])
     lista_de_campos = [
         {'key':'id_dispositivo','type':'str','fields':'DEVID'},
+        {'key':'versao','type':'str','fields':'VERSION'},
         {'key':'entradas_4a20','type':'4a20','fields':('LEVEL',)},
         {'key':'rele','type':'int','fields':('PUMP1','PUMP2',)},
         {'key':'status','type':'int','fields':('PUMP1','PUMP2',)},
         {'key':'pulso','type':'m3','fields':('VOLUME1','VOLUME2',)},
-        {'key':'ct_pulso','type':'m3','fields':('PERIOD1','PERIOD2',)},
+        {'key':'ct_pulso','type':'m3','fields':('FLOW1','FLOW2',)},
         {'key':'horimetro','type':'horimetro','fields':('HOURMETER1','HOURMETER2',)},
 
 
@@ -41,62 +42,65 @@ def on_message(client, userdata, msg):
     data_e_hora_atuais = datetime.now() 
     dict_save = {}
     dict_save['data_hora_dispositivo'] = data_e_hora_atuais.strftime('%Y-%m-%d %H:%M:%S')
-    for campo in lista_de_campos:
-        if isinstance(campo['fields'],tuple):
-            elementos_do_campo = []
-            if campo['type']=='4a20':
-                for field in campo['fields']:
-                    if field in dict_payload['DATA']:
-                        if field != "0":
-                            elementos_do_campo.append(str(float(dict_payload['DATA'][field])/100))
-                        else:
-                             elementos_do_campo.append(str(float(dict_payload['DATA'][field])))   
-            elif campo['type']=='m3':
-                for field in campo['fields']:
-                    if field in dict_payload['DATA']:
-                        if field != "0":
-                            elementos_do_campo.append(str(float(dict_payload['DATA'][field])/1000)) 
-                        else:    
-                            elementos_do_campo.append(dict_payload['DATA'][field]) 
-            elif campo['type']=='horimetro':
-                for field in campo['fields']:
-                    if field in dict_payload['DATA']:
-                        if field != "0":
-                            elementos_do_campo.append(str(float(dict_payload['DATA'][field])/10))  
-                        else:  
-                            elementos_do_campo.append(str(float(dict_payload['DATA'][field])))    
-            elif campo['type']=='pulso':
-                for field in campo['fields']:
-                    if field in dict_payload['DATA']:
-                        elementos_do_campo.append(int(dict_payload['DATA'][field]))                                     
+    try:
+        for campo in lista_de_campos:
+            if isinstance(campo['fields'],tuple):
+                elementos_do_campo = []
+                if campo['type']=='4a20':
+                    for field in campo['fields']:
+                        if field in dict_payload['DATA']:
+                            if field != "0":
+                                elementos_do_campo.append(str(float(dict_payload['DATA'][field])/100))
+                            else:
+                                elementos_do_campo.append(str(float(dict_payload['DATA'][field])))   
+                elif campo['type']=='m3':
+                    for field in campo['fields']:
+                        if field in dict_payload['DATA']:
+                            if field != "0":
+                                elementos_do_campo.append(str(float(dict_payload['DATA'][field])/1000)) 
+                            else:    
+                                elementos_do_campo.append(dict_payload['DATA'][field]) 
+                elif campo['type']=='horimetro':
+                    for field in campo['fields']:
+                        if field in dict_payload['DATA']:
+                            if field != "0":
+                                elementos_do_campo.append(str(float(dict_payload['DATA'][field])/100))  
+                            else:  
+                                elementos_do_campo.append(str(float(dict_payload['DATA'][field])))    
+                elif campo['type']=='pulso':
+                    for field in campo['fields']:
+                        if field in dict_payload['DATA']:
+                            elementos_do_campo.append(int(dict_payload['DATA'][field]))                                     
+                else:
+                    for field in campo['fields']:
+                        if field in dict_payload['DATA']:
+                            if campo['type'] == 'int':
+                                elementos_do_campo.append(int(dict_payload['DATA'][field]))   
+                            else:
+                                elementos_do_campo.append(str(dict_payload['DATA'][field]))   
+                dict_save[campo['key']] = elementos_do_campo
             else:
-                for field in campo['fields']:
-                    if field in dict_payload['DATA']:
-                        if campo['type'] == 'int':
-                            elementos_do_campo.append(int(dict_payload['DATA'][field]))   
-                        else:
-                            elementos_do_campo.append(str(dict_payload['DATA'][field]))   
-            dict_save[campo['key']] = elementos_do_campo
-        else:
-            if campo['type']=='int':
-                if campo['fields'] in dict_payload['DATA']:
-                    dict_save[campo['key']] = int(dict_payload['DATA'][campo['fields']])
-            else:
-                if campo['fields'] in dict_payload['DATA']:
-                    dict_save[campo['key']] = str(dict_payload['DATA'][campo['fields']])
-    dict_save['codigo_produto']= 19 
-    dict_save['timestamp_dispositivo'] = int(datetime.now().timestamp())
-    dict_save['timestamp_servidor'] = int(datetime.now().timestamp())
-    dict_save['dia_sem'] = calendar.day_name[dia_semana.weekday()]
-    #Remove chaves vazias do dicionario
-    remover_vazio = []
-    for chave, valor in dict_save.items():
-        if dict_save[chave] == []:
-            remover_vazio.append(chave)
-    for item in remover_vazio:
-        dict_save.pop(item)
-    print(dict_save)  
-    #queue.send_message(MessageBody=str(json.dumps(dict_save, ensure_ascii=False)))  
+                if campo['type']=='int':
+                    if campo['fields'] in dict_payload['DATA']:
+                        dict_save[campo['key']] = int(dict_payload['DATA'][campo['fields']])
+                else:
+                    if campo['fields'] in dict_payload['DATA']:
+                        dict_save[campo['key']] = str(dict_payload['DATA'][campo['fields']])
+        dict_save['codigo_produto']= 19 
+        dict_save['timestamp_dispositivo'] = int(datetime.now().timestamp())
+        dict_save['timestamp_servidor'] = int(datetime.now().timestamp())
+        dict_save['dia_sem'] = calendar.day_name[dia_semana.weekday()]
+        #Remove chaves vazias do dicionario
+        remover_vazio = []
+        for chave, valor in dict_save.items():
+            if dict_save[chave] == []:
+                remover_vazio.append(chave)
+        for item in remover_vazio:
+            dict_save.pop(item)
+        print(dict_save)  
+        queue.send_message(MessageBody=str(json.dumps(dict_save, ensure_ascii=False)))  
+    except:
+        pass    
 try:
     print('[STATUS] Inicializando MQTT...')
     #inicializa MQTT:
